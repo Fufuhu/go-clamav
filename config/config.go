c15cb1ef263ff2de4c20edca48fef13b34db0746
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2024-06-01 23:53:03 +0900

    設定をmainで取得するよう修正

--- config/config.go
+++ config/config.go
@@ -1,5 +1,16 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 type Configuration struct {
 	QueueURL string `envconfig:"QUEUE_URL" required:"true"`
+}
+
+func GetConfig() (*Configuration, error) {
+	conf := &Configuration{}
+	err := envconfig.Process("", conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
 }
--- main.go
+++ main.go
@@ -3,8 +3,21 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/Fufuhu/go-clamav/cmd"
+import (
+	"fmt"
+	"github.com/Fufuhu/go-clamav/cmd"
+	"github.com/Fufuhu/go-clamav/config"
+)
 
 func main() {
+
+	conf, err := config.GetConfig()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(conf.QueueURL)
+
 	cmd.Execute()
 }
