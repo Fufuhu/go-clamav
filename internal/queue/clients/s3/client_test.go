99d528ca8e3a43b88ed1de3a2688b725f811adbf
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2024-06-15 10:02:46 +0900

    S3オブジェクトの取り扱い処理を階層化した

--- internal/queue/clients/s3/client_test.go
+++ internal/queue/clients/s3/client_test.go
@@ -3,17 +3,45 @@ package s3
 import (
 	"context"
 	"github.com/Fufuhu/go-clamav/config"
+	"github.com/Fufuhu/go-clamav/internal/queue/clients"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewClient(t *testing.T) {
 	conf := config.Configuration{
-		S3BaseUrl: "http://localhost:9001",
+		S3BaseUrl: "http://localhost:9000",
 	}
 
 	ctx := context.Background()
 	client, err := NewClient(conf, ctx)
 	assert.NotNil(t, client)
 	assert.Nil(t, err)
+}
+
+// TestClient_ManipulateObject PutObject関数にてS3オブジェクトをアップロード、削除するテスト
+func TestClient_ManipulateObject(t *testing.T) {
+	conf := config.Configuration{
+		S3BaseUrl: "http://127.0.0.1:9000",
+		Region:    "ap-northeast-1",
+	}
+
+	ctx := context.Background()
+	client, err := NewClient(conf, ctx)
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+
+	t.Run("PutObject", func(t *testing.T) {
+		objectBody := []byte("test")
+		s3Object := clients.S3Object{
+			Bucket: "test",
+			Key:    "test",
+		}
+		err = client.PutObject(ctx, objectBody, s3Object)
+		assert.Nil(t, err)
+	})
+
+	t.Run("DeleteObject", func(t *testing.T) {
+		t.Skip("Skip DeleteObject test")
+	})
 }
