3f768e0fa77203f3d30f2de7e21463269f4b5115
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2024-06-15 23:18:24 +0900

    cleanなオブジェクトと感染していた場合のオブジェクトの場合の結果を作成

--- internal/db/clients/dynamodb/client.go
+++ internal/db/clients/dynamodb/client.go
@@ -9,6 +9,7 @@ import (
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	awsDynamodb "github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"go.uber.org/zap"
 )
 
 type Client struct {
@@ -43,9 +44,29 @@ func (c *Client) PutScanResult(ctx context.Context, result *model.ScanResult) (*
 	})
 
 	if err != nil {
-		logger.Warn("DynamoDBへのスキャン結果の追加に失敗しました")
+		logger.Warn("DynamoDBへのスキャン結果の追加に失敗しました",
+			zap.String("ObjectPath", objectPath))
 		logger.Error(err.Error())
 		return nil, err
+	}
+
+	// 感染していた場合
+	if result.ScanResult == model.ScanResultInfected {
+		logger.Info("感染しているファイルが検出されました",
+			zap.String("ObjectPath", objectPath))
+
+		// 情報を追加
+		_, err := c.service.PutItem(ctx, &awsDynamodb.PutItemInput{
+			TableName: aws.String(c.conf.DynamoDBTableInfected),
+			Item:      item,
+		})
+
+		if err != nil {
+			logger.Warn("DynamoDBへの感染ファイル情報の追加に失敗しました",
+				zap.String("ObjectPath", objectPath))
+			logger.Error(err.Error())
+			return nil, err
+		}
 	}
 
 	return result, nil
--- internal/db/clients/dynamodb/client_test.go
+++ internal/db/clients/dynamodb/client_test.go
@@ -12,26 +12,47 @@ import (
 // TestPutScanResult PutScanResult関数にてスキャン結果をDynamoDBに追加するテスト
 func TestPutScanResult(t *testing.T) {
 	conf := config.Configuration{
-		DynamoDBBaseUrl: "http://localhost:8000",
-		DynamoDBTable:   "ScanResults",
+		DynamoDBBaseUrl:       "http://localhost:8000",
+		DynamoDBTable:         "ScanResults",
+		DynamoDBTableInfected: "InfectedScanResults",
 	}
 
 	ctx := context.Background()
 	client, err := NewClient(conf, ctx)
 	assert.Nil(t, err)
 
-	result := &model.ScanResult{
-		S3Object: clients.S3Object{
-			Bucket: "test-bucket",
-			Key:    "test-key",
-		},
-		ScanResult: "clean",
-		ScannedAt:  "2021-01-01T00:00:00Z",
-	}
+	t.Run("clean", func(t *testing.T) {
 
-	result, err = client.PutScanResult(ctx, result)
-	assert.Nil(t, err)
-	assert.NotNil(t, result)
+		result := &model.ScanResult{
+			S3Object: clients.S3Object{
+				Bucket: "test-bucket",
+				Key:    "test-key-clean",
+			},
+			ScanResult: model.ScanResultClean,
+			ScannedAt:  "2021-01-01T00:00:00Z",
+		}
+
+		result, err = client.PutScanResult(ctx, result)
+		assert.Nil(t, err)
+		assert.NotNil(t, result)
+	})
+
+	t.Run("infected", func(t *testing.T) {
+
+		result := &model.ScanResult{
+			S3Object: clients.S3Object{
+				Bucket: "test-bucket",
+				Key:    "test-key-infected",
+			},
+			ScanResult: model.ScanResultInfected,
+			ScannedAt:  "2021-01-01T00:00:00Z",
+		}
+
+		result, err = client.PutScanResult(ctx, result)
+		assert.Nil(t, err)
+		assert.NotNil(t, result)
+	})
+
 }
 
 // TestNewClient NewClient関数にてDynamoDBクライアントを生成するテスト
