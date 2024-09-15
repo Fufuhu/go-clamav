9c6339c4e7ecd19b2e2f7568c88ce04e95ec9c97
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2024-09-15 14:34:52 +0900

    fix: インタフェースの誤りを修正

--- internal/queue/clients/interface.go
+++ internal/queue/clients/interface.go
@@ -3,7 +3,7 @@ package clients
 import "context"
 
 type ClientInterface interface {
-	Poll(ctx context.Context, process func(QueueMessageInterface) error) error
+	Poll(ctx context.Context, process func(QueueMessageInterface, context.Context) error) error
 	SendMessage(ctx context.Context, message string) (*string, error)
 	ReceiveMessages(ctx context.Context) ([]QueueMessageInterface, error)
 	DeleteMessage(ctx context.Context, receiptHandle string) error
