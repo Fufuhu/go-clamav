2c57c3d05177f552197a3a6a6a81a65f50b09b10
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2024-09-15 14:23:35 +0900

    feat: Scannerで実際にスキャンするよう修正

--- internal/virus_scan/scanner/scanner.go
+++ internal/virus_scan/scanner/scanner.go
@@ -3,10 +3,14 @@ package scanner
 import (
 	"context"
 	"github.com/Fufuhu/go-clamav/internal/db/clients/dynamodb"
+	"github.com/Fufuhu/go-clamav/internal/logging"
 	"github.com/Fufuhu/go-clamav/internal/model"
 	"github.com/Fufuhu/go-clamav/internal/objects/clients/s3"
 	"github.com/Fufuhu/go-clamav/internal/queue/clients"
 	"github.com/Fufuhu/go-clamav/internal/virus_scan/clients/clamav"
+	"go.uber.org/zap"
+	"io"
+	"time"
 )
 
 type Scanner struct {
@@ -20,6 +24,50 @@ func (s *Scanner) Scan() (model.ScanResult, error) {
 }
 
 func (s *Scanner) Process(message clients.QueueMessageInterface, ctx context.Context) error {
+	logger := logging.GetLogger()
+	defer logger.Sync()
+
+	file, err := s.s3Client.GetObject(ctx, message)
+	if err != nil {
+		logger.Warn("S3オブジェクトの取得に失敗しました")
+		logger.Error(err.Error())
+		return err
+	}
+	defer func(file io.ReadCloser) {
+		err := file.Close()
+		if err != nil {
+			logger.Error("ファイルのクローズに失敗しました")
+			logger.Error(err.Error())
+		}
+	}(file)
+
+	result, err := s.clamdClient.Scan(file)
+	if err != nil {
+		logger.Warn("ファイルのスキャンに失敗しました")
+		logger.Error(err.Error())
+		return err
+	}
+
+	scanResult := &model.ScanResult{}
+	scanResult.Key = message.GetKey()
+	scanResult.Bucket = message.GetBucket()
+	scanResult.ReceiptHandle = message.GetReceiptHandle()
+	scanResult.ScannedAt = time.Now().Format(time.RFC3339)
+
+	if result.Message == clamav.ResultOK {
+		logger.Info("ファイルは感染していません",
+			zap.String("Bucket", message.GetBucket()),
+			zap.String("Key", message.GetKey()))
+		scanResult.ScanResult = model.ScanResultClean
+	} else {
+		logger.Info("ファイルは感染しています",
+			zap.String("Bucket", message.GetBucket()),
+			zap.String("Key", message.GetKey()),
+			zap.String("Message", result.Message),
+		)
+		scanResult.ScanResult = model.ScanResultInfected
+	}
+
 	return nil
 }
 
